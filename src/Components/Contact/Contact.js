82761f24a1ce3da2c85c181b035cc034d0eb9cd5
avishekataur <avishekataur@gmail.com>
2021-11-30 18:59:35 +0600

    sizing-form

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -4,12 +4,15 @@ import { Col, Container, Form, Row, Button } from "react-bootstrap";
 const Contact = () => {
   return (
     <div className="pt-3 pb-5 text-dark">
-      <h2 className="text-center ">Contact Me</h2>
-      <Container>
+      <h2 className="text-center pb-2">Contact Me</h2>
+      <Container className="p-5">
         <Row>
           <Col lg={6}>
             <div>
-              <Form action="https://formsubmit.co/your@email.com" method="POST">
+              <Form
+                action="https://formsubmit.co/avishekataur@gmail.com"
+                method="POST"
+              >
                 <div className="d-flex mb-3">
                   <Form.Control
                     className="me-1 p-2"
@@ -58,7 +61,7 @@ const Contact = () => {
           </Col>
           <Col lg={6}>
             <div>
-              <h4>Follow Me</h4>
+              <h4 className="text-center">Follow Me</h4>
             </div>
           </Col>
         </Row>
