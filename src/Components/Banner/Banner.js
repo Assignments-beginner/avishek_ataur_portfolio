bdc7549ddc6f5b5531b81fc6127f3b45812b8649
avishekataur <avishekataur@gmail.com>
2021-11-30 10:17:21 +0600

    banner-sizing-done

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,27 +1,28 @@
 import React from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
-import '../Banner/Banner.css'
+import "../Banner/Banner.css";
 import profile from "../../Images/Profile.jpg";
 
 const Banner = () => {
   return (
     <div>
       <Container className="py-5">
-        <Row className="align-items-center">
-          <Col sm={6}>
-            <div>
+        <Row className="align-items-center justify-content-center">
+          <Col className="d-flex justify-content-end" sm={5}>
+            <div className="me-5">
               <img className="profile-img" src={profile} alt="" />
             </div>
           </Col>
-          <Col sm={6} className="text-dark">
-            <h1 className="text-uppercase">Junior Web Developer</h1>
+          <Col sm={7} className="text-dark">
+            <h4 className="text-uppercase">Junior Web Developer</h4>
+            <h1>Avishek Ataur</h1>
             <p>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque
               nulla voluptatibus voluptatem nisi nam molestiae quia cumque,
               voluptate repellendus consequuntur odit harum, porro voluptatum
               non vel dolor, inventore sit. Temporibus.
             </p>
-            <Button variant="secondary">Download Resume</Button>
+            <Button className="mt-3" variant="outline-secondary">Download Resume</Button>
           </Col>
         </Row>
       </Container>
