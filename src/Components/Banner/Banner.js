c37ed2dfe0fe2a4291a7cdadec2c509887feee95
avishekataur <avishekataur@gmail.com>
2021-12-02 08:36:24 +0600

    heading-animation-done

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,18 +1,9 @@
-import React, { useEffect } from "react";
-import Aos from "aos";
-import "aos/dist/aos.css";
+import React from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
 import "../Banner/Banner.css";
 import profile from "../../Images/Profile.jpg";
 
 const Banner = () => {
-  useEffect(() => {
-    Aos.init({
-      duration: 1000,
-      once: false,
-      mirror: true,
-    });
-  }, []);
   const resume = () => {
     const url =
       "https://drive.google.com/file/d/1VtNwXxNhyV4xhHvTVh6HI8UcaFaWDFY-/view?usp=sharing";
@@ -33,9 +24,8 @@ const Banner = () => {
               <div class="fadingEffect"></div>
             </div>
 
-            <div data-aos="fade-right" data-aos-mirror="true">
-              <h1>Avishek Ataur</h1>
-            </div>
+            <h1 className="text-dark profile-name display-5">Avishek Ataur</h1>
+
             <p className="mt-4 pt-2">
               <strong>Hello! Everyone,</strong> <br />
               I'm a Junior Web Developer with five plus months of experience
