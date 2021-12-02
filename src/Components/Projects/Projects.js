e3b233d94631933a85c095915f2b5bab4020a8e6
avishekataur <avishekataur@gmail.com>
2021-12-02 15:02:23 +0600

    carousel-fixed

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,48 +1,41 @@
 import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-// import Slider from "react-slick";
-// import { baseUrl } from "../../baseUrl";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import img1 from "../../Images/slides/abstract01.jpg";
+import img2 from "../../Images/slides/abstract02.jpg";
+import img3 from "../../Images/slides/abstract03.jpg";
 
 const Projects = () => {
   useEffect(() => {
     AOS.init({ duration: 2000 });
   }, []);
-  /* const settings = {
-    customPaging: function (i) {
-      return (
-        <div>
-          <img src={`${baseUrl}/abstract0${i + 1}.jpg`} alt="" />
-        </div>
-      );
-    },
-    dots: true,
-    dotsClass: "slick-dots slick-thumb",
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  }; */
+
   return (
     <div id="projects" className="pb-5 px-5 mx-5">
       <div data-aos="fade-left">
         <h2 className="text-center text-dark pb-2">Personal Projects</h2>
       </div>
 
-      <h4 className="text-center text-dark">
+      {/* <h4 className="text-center text-dark">
         Not Completed Yet <span className="display-4">😔</span>
-      </h4>
-      {/* <Slider className="px-5 border border-dark" {...settings}>
+      </h4> */}
+
+      <Carousel infiniteLoop autoPlay>
         <div>
-          <img src={abstract01} alt="" />
+          <img src={img1} alt="" />
+          <p className="legend">Legend 1</p>
         </div>
         <div>
-          <img src={baseUrl + "/abstract02.jpg"} alt="" />
+          <img src={img2} alt="" />
+          <p className="legend">Legend 2</p>
         </div>
         <div>
-          <img src={baseUrl + "/abstract03.jpg"} alt="" />
+          <img src={img3} alt="" />
+          <p className="legend">Legend 3</p>
         </div>
-      </Slider> */}
+      </Carousel>
     </div>
   );
 };
