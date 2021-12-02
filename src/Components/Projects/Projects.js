73a797d985e2f6739ca840c38166fd0be2a6a487
avishekataur <avishekataur@gmail.com>
2021-12-02 15:37:32 +0600

    arrow-removed

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -6,6 +6,8 @@ import { Carousel } from "react-responsive-carousel";
 import img1 from "../../Images/slides/abstract01.jpg";
 import img2 from "../../Images/slides/abstract02.jpg";
 import img3 from "../../Images/slides/abstract03.jpg";
+import "../Projects/Projects.css";
+import { Button } from "react-bootstrap";
 
 const Projects = () => {
   useEffect(() => {
@@ -15,25 +17,23 @@ const Projects = () => {
   return (
     <div id="projects" className="pb-5 px-5 mx-5">
       <div data-aos="fade-left">
-        <h2 className="text-center text-dark pb-2">Personal Projects</h2>
+        <h2 className="text-center text-dark pb-5 mb-4">Personal Projects</h2>
       </div>
 
       {/* <h4 className="text-center text-dark">
         Not Completed Yet <span className="display-4">😔</span>
       </h4> */}
 
-      <Carousel infiniteLoop autoPlay>
-        <div>
+      <Carousel swipeable useKeyboardArrows infiniteLoop autoPlay>
+        <div className="d-grid justify-content-center">
           <img src={img1} alt="" />
-          <p className="legend">Legend 1</p>
+          <span className="Legend text-dark">Details</span>
         </div>
         <div>
           <img src={img2} alt="" />
-          <p className="legend">Legend 2</p>
         </div>
         <div>
           <img src={img3} alt="" />
-          <p className="legend">Legend 3</p>
         </div>
       </Carousel>
     </div>
