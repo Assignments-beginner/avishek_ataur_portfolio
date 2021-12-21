deafffb81fc84d0b6553e831ee41392ccfbb8a27
avishekataur <avishekataur@gmail.com>
2021-12-21 15:10:08 +0600

    blog-css

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import "../Blogs/Blogs.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { Card, Col, Container, Row, Button } from "react-bootstrap";
+import cssThumbnail from "../Blogs/Css/css_img/css_thumbnail.png";
 
 const Blogs = () => {
   useEffect(() => {
@@ -10,13 +12,39 @@ const Blogs = () => {
   return (
     <div className="blogs">
       <div data-aos="fade-right">
-        <h2 className="text-dark text-center res-blog-top pb-2">Blog</h2>
+        <h2 className="text-dark text-center res-blog-top pb-5">Blog</h2>
       </div>
-      <div data-aos="fade-left">
+      {/* <div data-aos="fade-left">
         <h1 className="text-center text-uppercase text-dark mt-5 res-blog-header">
           coming soon...
         </h1>
-      </div>
+      </div> */}
+      <Container className="px-5">
+        <Row>
+          <Col xl={3} lg={3} sm={12}>
+            <Card>
+              <Card.Img variant="top" src={cssThumbnail} />
+              <Card.Body>
+                <Card.Title className="text-dark text-center">
+                  Card title
+                </Card.Title>
+                <Card.Text className="text-dark text-center">
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+                <Button size="sm" variant="secondary">
+                  Details
+                </Button>
+              </Card.Body>
+              <Card.Footer className="text-secondary text-center">
+                <small>12 / 21 / 2021</small>
+              </Card.Footer>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+      {/* footer */}
       <div>
         <hr className="new mt-5" />
         <p className="mt-2 text-secondary text-center text-uppercase">
---
+++ src/Components/Blogs/Css/Css.js
@@ -0,0 +1,114 @@
+import React from "react";
+import "../Css/Css.css";
+import p1 from "../Css/css_img/barca/p1.webp";
+import p2 from "../Css/css_img/barca/p2.webp";
+import p3 from "../Css/css_img/barca/p3.webp";
+import player1 from "../Css/css_img/Players/Player1.webp";
+import player2 from "../Css/css_img/Players/Player2.webp";
+import player3 from "../Css/css_img/Players/Player3.webp";
+
+const Css = () => {
+  return (
+    <React.Fragment>
+      {/* <!--------------------------------------------------------------------------------- 
+  /////////////////////////////// in-Frame \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\  
+ ----------------------------------------------------------------------------------> */}
+      <h1 style={{ textAlign: "center", fontSize: "60px" }}>In-Frame</h1>
+      {/* <!-- container  --> */}
+
+      <div class="container">
+        {/* <!-- frame  --> */}
+        <div class="frame">
+          <div class="photo">
+            <img src={p1} alt="" />
+          </div>
+
+          <div class="info">
+            <h1>La Chandela</h1>
+            <h3>Antra del capanila</h3>
+          </div>
+        </div>
+        {/* <!-- frame  --> */}
+        <div class="frame">
+          <div class="photo">
+            <img src={p2} alt="" />
+          </div>
+
+          <div class="info">
+            <h1>La Chandela</h1>
+            <h3>Antra del capanila</h3>
+          </div>
+        </div>
+        {/* <!-- frame  --> */}
+        <div class="frame">
+          <div class="photo">
+            <img src={p3} alt="" />
+          </div>
+
+          <div class="info">
+            <h1>La Chandela</h1>
+            <h3>Antra del capanila</h3>
+          </div>
+        </div>
+
+        {/* <!-- container  --> */}
+      </div>
+      {/* <!--------------------------------------------------------------------------------- 
+  ////////////////////////////// Without-Frame \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\  
+ ----------------------------------------------------------------------------------> */}
+      <br />
+      <h1 style={{ textAlign: "center", fontSize: "60px" }}>Without-Frame</h1>
+      <br />
+      <br />
+
+      <div class="container-2">
+        {/* <!-- container  --> */}
+        {/* <!-- frame  --> */}
+        <div class="frame-2">
+          {/* <!-- frame  --> */}
+          <div class="photo-2">
+            <img src={player1} alt="" />
+          </div>
+
+          <div class="info-2">
+            <h2>La Chandela</h2>
+            <h3>Antra del capanila</h3>
+          </div>
+        </div>
+        {/* <!-- frame  --> */}
+        <div class="frame-2">
+          {/* <!-- frame  --> */}
+          <div class="photo-2">
+            <img src={player2} alt="" />
+          </div>
+
+          <div class="info-2">
+            <h2>La Chandela</h2>
+            <h3>Antra del capanila</h3>
+          </div>
+        </div>
+        {/* <!-- frame  --> */}
+        <div class="frame-2">
+          {/* <!-- frame  --> */}
+          <div class="photo-2">
+            <img src={player3} alt="" />
+          </div>
+
+          <div class="info-2">
+            <h2>La Chandela</h2>
+            <h3>Antra del capanila</h3>
+          </div>
+        </div>
+
+        {/* <!-- container  --> */}
+      </div>
+      {/* <!-- line-breaks  --> */}
+      <br />
+      <br />
+      <br />
+      <br />
+    </React.Fragment>
+  );
+};
+
+export default Css;
